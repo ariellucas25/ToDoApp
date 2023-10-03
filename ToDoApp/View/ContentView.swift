7cd65fa8bd6a2b06ce0eec624ce6803e4bfc9773
Ariel Lucas Luduvig <ariellucas25@gmail.com>
2023-10-03 20:49:42 -0300

    . Added NewTaskView - that appear just when the "New Task Button" is pressed

--- ToDoApp/View/ContentView.swift
+++ ToDoApp/View/ContentView.swift
@@ -12,10 +12,8 @@ struct ContentView: View {
     //    MARK: PROPERTY
     
     @State var task: String = ""
+    @State private var showNewTaskItem: Bool = false
     
-    private var isButtonDisable: Bool {
-        task.isEmpty
-    }
     
     //    FETCHING DATA
     @Environment(\.managedObjectContext) private var viewContext
@@ -27,24 +25,7 @@ struct ContentView: View {
     
     //    MARK: - FUNCTION
     
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-            newItem.task = task
-            newItem.completion = false
-            newItem.id = UUID()
-            
-            do {
-                try viewContext.save()
-            } catch {
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-            task = ""
-            hideKeyboard()
-        }
-    }
+
     
     private func deleteItems(offsets: IndexSet) {
         withAnimation {
@@ -62,31 +43,34 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             ZStack {
+                // MARK: - MAIN VIEW
                 VStack {
-                    VStack(spacing: 16){
-                        TextField("New Task", text: $task)
-                            .padding()
-                            .background(
-                                Color(UIColor.systemGray6)
-                            )
-                            .cornerRadius(10)
-                        
-                        Button(action: {
-                            addItem()
-                        }, label: {
-                            Spacer()
-                            Text("SAVE")
-                            Spacer()
-                        })
-                        .disabled(isButtonDisable)
-                        .padding()
-                        .font(.headline)
-                        .foregroundColor(.white)
-                        .background(isButtonDisable ? Color.gray : Color.pink)
-                        .cornerRadius(10)
-
-                    }//: VSTACK
-                    .padding()
+                    // MARK: - HEADER
+                    Spacer(minLength: 80)
+                    
+                    // MARK: - NEW TASK BUTTON
+                    Button(action: {
+                        showNewTaskItem = true
+                    }, label: {
+                        Image(systemName: "plus.circle")
+                            .font(.system(size: 30, weight: .semibold, design: .rounded
+                                         ))
+                        Text("New Task")
+                            .font(.system(size: 24, weight: .bold, design: .rounded))
+                    })
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 15)
+                    .background(
+                        LinearGradient(gradient: Gradient(colors: [Color.pink, Color.blue]), startPoint: .leading, endPoint: .trailing)
+                            .clipShape(Capsule())
+                    )
+                    
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.25), radius: 8, x: 0.0, y: 4.0)
+                    
+                    
+                
+                    //MARK: - TASKS
                 
                     List {
                         ForEach(items) { item in
@@ -107,6 +91,19 @@ struct ContentView: View {
                     .padding(.vertical, 0)
                     .frame(maxWidth: 640)
                 }//: VSTACK
+                
+                //MARK: - NEW TASK ITEM
+                if showNewTaskItem {
+                    BlankView()
+                        .onTapGesture {
+                            withAnimation() {
+                                showNewTaskItem = false
+                            }
+                        }
+                    NewTaskItemView(isShowing: $showNewTaskItem)
+                }
+                
+                
             }//: ZSTACK
         
             .navigationBarTitle("Daily Tasks", displayMode: .large)
@@ -118,12 +115,14 @@ struct ContentView: View {
                 #endif
         }//:TOOLBAR
             
-            .background(
-                BackgroundImageView()
-            )
+            
             .background(
                 backgroundGradient.ignoresSafeArea(.all)
                 )
+            
+            .background(
+                BackgroundImageView()
+            )
             
         }//: NAVIGATION
         .navigationViewStyle(StackNavigationViewStyle())
